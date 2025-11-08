94cf40dc853a17b53e3f9bc7f964784488c956d9
duck-bongos <billmannd@gmail.com>
2025-11-08 13:58:49 -0500

    specified public and copy directories for github actions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use dioxus::prelude::*;
 
-const FAVICON: Asset = asset!("/assets/favicon.ico");
-const MAIN_CSS: Asset = asset!("/assets/main.css");
 const HEADER_SVG: Asset = asset!("/assets/header.svg");
 
 fn main() {
@@ -11,8 +9,8 @@ fn main() {
 #[component]
 fn App() -> Element {
     rsx! {
-        document::Link { rel: "icon", href: FAVICON }
-        document::Link { rel: "stylesheet", href: MAIN_CSS }
+        document::Link { rel: "icon", href: "https://dan-codes-badly.com/assets/favicon_cyan/favicon.ico", type:"image/x-icon" }
+        document::Link { rel: "stylesheet", href: "https://dan-codes-badly.com/assets/theme.css" }
         Hero {}
 
     }
