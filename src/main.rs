2447561ac19bd5d69e82aa216613b39c8f67efbb
duck-bongos <billmannd@gmail.com>
2025-11-17 19:20:53 -0500

    do NOT route '/'

--- src/main.rs
+++ src/main.rs
@@ -219,11 +219,11 @@ fn App() -> impl IntoView {
         <Navbar/>
         <Router>
             <Routes fallback=|| "Not Found">
-            <Route path=path!("/") view=Protein/>
+            // <Route path=path!("/") view=Protein/>
             <Route path=path!("/protein-comparison-tool") view=Protein/>
             <Route path=path!("/cat") view=|| view! {<h1>"CAT"</h1>}/>
             <Route path=path!("/dog") view=|| view! {<h1>"DOG"</h1>}/>
-            <Route path=path!("/*any") view=|| "Not Found"/>
+            // <Route path=path!("/*any") view=|| "Not Found"/>
             </Routes>
         </Router>
     }
